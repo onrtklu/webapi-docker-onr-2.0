ed05e711937cdf02bb28b7f8729564e9891871f8
onrtklu <onur_toklu_@hotmail.com>
2020-02-25 17:26:09 +0300

    trigger test 1

--- src/OnrWebApi/Controllers/ValuesController.cs
+++ src/OnrWebApi/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace OnrWebApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value2", "value3" };
         }
 
         // GET api/values/5
